dff57db07c30e4b303545c123773b15e3c16d884
Menezes-jom <joseosmanmenezes@gmail.com>
2025-12-15 13:39:42 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,198 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8"/>
+    <title>EFA</title>
+    <style type="text/css" >
+		.novoj{
+        padding: 1%;
+        font-family: verdana;
+        font-size: 100%;
+        margin-left: 7%;
+        border-radius: 5px;
+        border-color: #4F4F4F;
+        font-family: 'Press Start 2P', cursive;
+    	}
+    	.novoj:hover {
+        background-color:   #CD950C; 
+        color: white;
+		}	
+		
+		.fcc-btn {
+		  background-color: #4F4F4F;
+		  color: white;
+		  padding: 15px 25px;
+		  text-decoration: none;		  	  
+		}
+		
+		.fcc-btn1 {
+		  background-color: #C0C0C0;
+		  color: white;
+		  padding: 15px 25px;
+		  text-decoration: none;
+		}
+		
+		div { 
+		  width: 70%; 
+		  background: skyblue;
+		}
+		
+		.div2 {
+			width: 70%;
+			background: #3399FF;
+		}
+	</style>
+</head>
+
+<body>
+<form action="search.php" method="post">
+<center>
+<div>
+<h1><center>ESCOLA DE EVANGELHO E DOUTRINA ESPÍRITA <br> FRANCISCO DE ASSIS </center> </h1>
+<h2><center>LISTAGEM DAS TURMAS PARA 2024</center></h2>
+</div>
+
+<div align="left">
+<br>
+
+<a href="../efa/Ementas_2024.pdf" class="fcc-btn">Ementas 2024</a>
+<a href="search.php" class="fcc-btn">Consulta por nome....</a>
+
+<br><br>
+</div>
+<div class="div2"><h2>DOMINGO 07:00 - 08:30 </h2></div><br>
+</center>
+<div2> 
+<center>
+    <a href="../Domingo_07horas_Sala01_Principios_Basicos.pdf" class="fcc-btn1">01) Domingo 07:00 Sala 01 Princípios Básicos</a><br><br><br>
+
+<a href="../Domingo_07horas_Sala03_Principios_Basicos.pdf" class="fcc-btn">02) Domingo 07:00 Sala 03 Princípios Básicos</a><br><br><br>
+
+<a href="../Domingo_07horas_Sala04_Livro_Espiritos.pdf" class="fcc-btn1">03) Domingo 07:00 Sala 04 Livro dos Espíritos</a><br><br><br>
+
+<a href="../Domingo_07horas_Sala05_Livro_Espiritos.pdf" class="fcc-btn">04) Domingo 07:00 Sala 05 Livro dos Espíritos</a><br><br><br>
+
+<a href="../Domingo_07horas_Sala06_Livro_Espiritos.pdf" class="fcc-btn1">05) Domingo 07:00 Sala 06 Livro dos Espíritos</a><br><br><br>
+
+<a href="../Domingo_07horas_Sala07_Principios_Basicos.pdf" class="fcc-btn">06) Domingo 07:00 Sala 07 Princípios Básicos</a><br><br><br>
+
+<a href="../Domingo_07horas_Sala08_Mediunidade_Cristo.pdf" class="fcc-btn1">07) Domingo 07:00 Sala 08 Mediunidade com Cristo</a><br><br><br>
+
+<a href="../Domingo_07horas_Sala09_Evangelho.pdf" class="fcc-btn1">08) Domingo 07:00 Sala 09 Evangelho Segundo Espiritismo</a><br><br><br>
+
+<a href="../Domingo_07horas_Sala12_Principios_Basicos.pdf" class="fcc-btn">09) Domingo 07:00 Sala 12 Princípios Básicos</a><br><br><br>
+
+<div class="div2"><h2>DOMINGO 08:30 - 10:00 </h2></div><br>
+
+<a href="../Domingo_08horas_Sala01_Principios_Basicos.pdf" class="fcc-btn1">10) Domingo 08:30 Sala 01 Princípios Básicos</a><br><br><br>
+
+<a href="../Domingo_08horas_Sala03_Livro_Espiritos.pdf" class="fcc-btn">11) Domingo 08:30 Sala 03 Livro dos Espíritos</a><br><br><br>
+
+<a href="../Domingo_08horas_Sala04_Principios_Basicos.pdf" class="fcc-btn1">12) Domingo 08:30 Sala 04 Princípios Básicos</a><br><br><br>
+
+<a href="../Domingo_08horas_Sala05_Nosso_Lar.pdf" class="fcc-btn">13) Domingo 08:30 Sala 05 Nosso Lar</a><br><br><br>
+
+<a href="../Domingo_08horas_Sala06_Livro_Espiritos.pdf" class="fcc-btn1">14) Domingo 08:30 Sala 06 Livro dos Espíritos</a><br><br><br>
+
+<a href="../Domingo_08horas_Sala07_Principios_Basicos.pdf" class="fcc-btn">15) Domingo 08:30 Sala 07 Princípios Básicos</a><br><br><br>
+
+<a href="../Domingo_08horas_Sala08_Principios_Basicos.pdf" class="fcc-btn1">16) Domingo 08:30 Sala 08 Princípios Básicos</a><br><br><br>
+
+<a href="../Domingo_08horas_Sala09_Principios_Basicos.pdf" class="fcc-btn">17) Domingo 08:30 Sala 09 Princípios Básicos</a><br><br><br>
+
+<a href="../Domingo_08horas_Sala12_Principios_Basicos.pdf" class="fcc-btn1">18) Domingo 08:30 Sala 12 Princípios Básicos</a><br><br><br>
+
+<a href="../Domingo_08horas_SalaAU_Livro_Espiritos.pdf" class="fcc-btn">19) Domingo 08:30 Auditório Livro dos Espíritos</a><br><br><br>
+
+<div class="div2"><h2>DOMINGO 10:00 - 11:30 </h2></div><br>
+
+<a href="../Domingo_10horas_Sala01_Principios_Basicos.pdf" class="fcc-btn1">20) Domingo 10:00 Sala 01 Princípios Básicos</a><br><br><br>
+
+<a href="../Domingo_10horas_Sala03_Principios_Basicos.pdf" class="fcc-btn">21) Domingo 10:00 Sala 03 Princípios Básicos</a><br><br><br>
+
+<a href="../Domingo_10horas_Sala04_Principios_Basicos.pdf" class="fcc-btn1">22) Domingo 10:00 Sala 04 Princípios Básicos</a><br><br><br>
+
+<a href="../Domingo_10horas_Sala05_Principios_Basicos.pdf" class="fcc-btn">23) Domingo 10:00 Sala 05 Princípios Básicos</a><br><br><br>
+
+<a href="../Domingo_10horas_Sala06_Principios_Basicos.pdf" class="fcc-btn1">24) Domingo 10:00 Sala 06 Princípios Básicos</a><br><br><br>
+
+<a href="../Domingo_10horas_Sala07_Livro_Espiritos.pdf" class="fcc-btn">25) Domingo 10:00 Sala 07 Livro dos Espíritos</a><br><br><br>
+
+<a href="../Domingo_10horas_Sala08_Principios_Basicos.pdf" class="fcc-btn1">26) Domingo 10:00 Sala 08 Princípios Básicos</a><br><br><br>
+
+<a href="../Domingo_10horas_Sala09_Principios_Basicos.pdf" class="fcc-btn">27) Domingo 10:00 Sala 09 Princípios Básicos</a><br><br><br>
+
+<a href="../Domingo_10horas_Sala12_Principios_Basicos.pdf" class="fcc-btn1">28) Domingo 10:00 Sala 12 Princípios Básicos</a><br><br><br>
+
+<a href="../Domingo_10horas_SalaAU_Livro_Espiritos.pdf" class="fcc-btn">29) Domingo 10:00 Auditório Livro dos Espíritos</a><br><br><br>
+
+<div class="div2"><h2>QUARTA 08:30 - 10:00 </h2></div><br>
+
+<a href="../Quarta_08horas_Sala06_Principios_Basicos.pdf" class="fcc-btn1">30) Quarta 08:30 Sala 06 Princípios Básicos</a><br><br><br>
+
+<div class="div2"><h2>QUARTA 13:00 - 14:30 </h2></div><br>
+
+<a href="../Quarta_13horas_Sala01_Livro_Espiritos.pdf" class="fcc-btn">31) Quarta 13:00 Sala 01 Livro dos Espíritos</a><br><br><br>
+
+<a href="../Quarta_13horas_Sala03_Evangelho.pdf" class="fcc-btn1">32) Quarta 13:00 Sala 03 Evangelho Segundo Espitismo</a><br><br><br>
+
+<a href="../Quarta_13horas_Sala05_Livro_Espirito.pdf" class="fcc-btn">33) Quarta 13:00 Sala 05 Livro dos Espíritos</a><br><br><br>
+
+<a href="../Quarta_13horas_Sala06_Evangelho.pdf" class="fcc-btn1">34) Quarta 13:00 Sala 06 Evangelho Segundo Espiritismo</a><br><br><br>
+
+<a href="../Quarta_13horas_Sala07_Principios_Basicos.pdf" class="fcc-btn">35) Quarta 13:00 Sala 07 Princípios Básicos</a><br><br><br>
+
+<a href="../Quarta_13horas_Sala12_Principios_Basicos.pdf" class="fcc-btn1">36) Quarta 13:00 Sala 12 Princípios Básicos</a><br><br><br>
+
+<a href="../Quarta_13horas_SalaAU_Mediunidade_Cristo.pdf" class="fcc-btn">37) Quarta 13:00 Auditório Mediunidade com Cristo</a><br><br><br>
+
+<div class="div2"><h2>QUARTA 14:30 - 16:00 </h2></div><br>
+
+<a href="../Quarta_14horas_Sala01_Mediunidade_Cristo.pdf" class="fcc-btn1">38) Quarta 14:30 Sala 01 Mediunidade com Cristo</a><br><br><br>
+
+<a href="../Quarta_14horas_Sala03_Livro_Espirito.pdf" class="fcc-btn">39) Quarta 14:30 Sala 03 Livro dos Espíritos</a><br><br><br>
+
+<a href="../Quarta_14horas_Sala04_Principios_Basicos.pdf" class="fcc-btn1">40) Quarta 14:30 Sala 04 Princípios Básicos</a><br><br><br>
+
+<a href="../Quarta_14horas_Sala05_Livro_Espirito.pdf" class="fcc-btn">41) Quarta 14:30 Sala 05 Livro dos Espíritos</a><br><br><br>
+
+<a href="../Quarta_14horas_Sala07_Principios_Basicos.pdf" class="fcc-btn1">42) Quarta 14:30 Sala 07 Princípios Básicos</a><br><br><br>
+
+<a href="../Quarta_14horas_Sala12_Livro_Espirito.pdf" class="fcc-btn">43) Quarta 14:30 Sala 12 Livro dos Espíritos</a><br><br><br>
+
+<a href="../Quarta_14horas_SalaAU_Evangelho.pdf" class="fcc-btn1">44) Quarta 14:30 Auditório Evangelho Segundo Espiritismo</a><br><br><br>
+
+<div class="div2"><h2>QUARTA 16:00 - 17:30 </h2></div><br>
+
+<a href="../Quarta_16horas_Sala03_Principios_Basicos.pdf" class="fcc-btn">45) Quarta 16:00 Sala 03 Princípios Básicos</a><br><br><br>
+
+<a href="../Quarta_16horas_Sala04_Nosso_Lar.pdf" class="fcc-btn1">46) Quarta 16:00 Sala 04 Nosso Lar</a><br><br><br>
+
+<a href="../Quarta_16horas_Sala05_Livro_Espirito.pdf" class="fcc-btn">47) Quarta 16:00 Sala 05 Livro dos Espíritos</a><br><br><br>
+
+<a href="../Quarta_16horas_Sala06_Evangelho.pdf" class="fcc-btn1">48) Quarta 16:00 Sala 06 Evangelho Segundo Espiritismo</a><br><br><br>
+
+<a href="../Quarta_16horas_Sala12_Principios_Basicos.pdf" class="fcc-btn">49) Quarta 16:00 Sala 12 Princípios Básicos</a><br><br><br>
+
+<div class="div2"><h2>QUARTA 17:30 - 19:00 </h2></div><br>
+
+<a href="../Quarta_17horas_Sala03_Principios_Basicos.pdf" class="fcc-btn1">50) Quarta 17:30 Sala 03 Princípios Básicos</a><br><br><br>
+
+<a href="../Quarta_17horas_Sala04_Principios_Basicos.pdf" class="fcc-btn">51) Quarta 17:30 Sala 04 Princípios Básicos</a><br><br><br>
+
+<a href="../Quarta_17horas_Sala06_Livro_Espirito.pdf" class="fcc-btn1">52) Quarta 17:30 Sala 06 Livro dos Espíritos</a><br><br><br>
+
+<a href="../Quarta_17horas_SalaAU_Evangelho.pdf" class="fcc-btn">53) Quarta 17:30 Auditório Evangelho Segundo Espiritismo</a><br><br><br>
+
+<div class="div2"><h2>QUINTA 14:30- 16:00 </h2></div><br>
+
+<a href="../Quinta_14horas_Sala01_Principios_Basicos.pdf" class="fcc-btn1">54) Quarta 14:30 Sala 1 Princípios Básicos</a><br><br><br>
+
+
+</center>
+</form>
+
+
+</body>
+
+</html>
